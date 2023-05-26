885aa8134742788ae5e1461342aba22a6b494487
Ryan Krokson <Ryan.Krokson@Microsoft.com>
2023-05-26 15:24:45 -0500

    updating mysql script

--- PLS-for-onprem-workloads-withADF-haproxy/linked/mysql-scripts.sh
+++ PLS-for-onprem-workloads-withADF-haproxy/linked/mysql-scripts.sh
@@ -6,8 +6,8 @@ sed -i '/bind-address/s/^/#/g' /etc/mysql/mysql.conf.d/mysqld.cnf
 # restart mysql
 service mysql restart
 
-# connect to mysql and create a database
-# mysql -u root -p
-# create database testdb;
-# create user 'mysqluser'@'localhost' identified by 'password';
-# grant all on testdb.* to 'mysqluser'@'%' identified by 'password';
+# connect to mysql, create a database, give privileges to a user (change password to your password)
+sudo mysql -u root
+create database testdb;
+create user 'mysqluser'@'localhost' identified by 'password';
+grant all on testdb.* to 'mysqluser'@'%' identified by 'password';
